6f945ce5bb697453106cc5b098ca35f42a499ad4
Daniel <danielbarros338@gmail.com>
2023-08-21 16:41:06 -0300

    tensorflow configurado

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    head: {
+      script: [{
+        src: "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-vis@1.0.2/dist/tfjs-vis.umd.min.js"
+      }]
+    }
+  }
 })
