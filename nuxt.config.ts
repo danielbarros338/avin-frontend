00d0eed3a277186bc786a50ad0de600a5cfba9ee
Daniel <danielbarros338@gmail.com>
2023-08-22 08:34:23 -0300

    configuracao do vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,5 +7,17 @@ export default defineNuxtConfig({
         src: "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-vis@1.0.2/dist/tfjs-vis.umd.min.js"
       }]
     }
+  },
+  css: [
+    'vuetify/lib/styles/main.sass',
+    '@mdi/font/css/materialdesignicons.min.css'
+  ],
+  build: {
+    transpile: ['vuetify']
+  },
+  vite: {
+    define: {
+      'process.env.DEBUG': false
+    }
   }
 })
