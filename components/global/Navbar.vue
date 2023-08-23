ca9cf01168b5d51b91989fcc3dab38e09f82337c
Daniel <danielbarros338@gmail.com>
2023-08-23 09:48:14 -0300

    Criado menu desktop

--- components/global/Navbar.vue
+++ components/global/Navbar.vue
@@ -1,11 +1,52 @@
 <template>
   <v-app-bar style="background: #3949AB">
-    <div class="d-flex justify-space-between align-center" style="width: 100%">
-      <div id="portal-icon" @click="sendHome">
+    <div class="d-flex justify-space-between justify-md-start align-center px-2" style="width: 100%">
+      <div id="portal-icon" @click="sendHome" style="width: 10%">
         <img src="/img/avin-sm-logo.png" />
       </div>
-    
 
+      <!-- Desktop -->
+      <div
+        v-if="!isMobile"
+        class="ml-5"
+      >
+        <v-btn
+          class="elevation-1 ml-2"
+          color="#8cae61"
+          variant="elevated"
+          to="/acoes"
+        >
+          <v-icon>mdi-finance</v-icon>
+          Ações
+        </v-btn>
+        <v-btn
+          class="elevation-1 ml-2"
+          color="#8cae61"
+          variant="elevated"
+          to="/criptomoedas"
+        >
+          <v-icon>mdi-bitcoin</v-icon>
+          Criptomoedas
+        </v-btn>
+        <v-btn
+          class="elevation-1 ml-2"
+          color="#8cae61"
+          variant="elevated"
+          to="/inflacao"
+        >
+          <v-icon>mdi-chart-line</v-icon>
+          Inflação
+        </v-btn>
+        <v-btn
+          class="elevation-1 ml-2"
+          color="#8cae61"
+          variant="elevated"
+          to="/selic"
+        >
+          <v-icon>mdi-chart-bar</v-icon>
+          Selic
+        </v-btn>
+      </div>
       <!-- Mobile -->
 
       <v-btn
@@ -64,7 +105,7 @@ onMounted(() => {
 })
 
 function handleResize() {
-  isMobile.value = window.innerWidth < 800;
+  isMobile.value = window.innerWidth < 960;
 }
 
 function sendHome() {
