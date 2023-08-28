bd20c5e3ab89c5e27feace48be59a1e15cd5a660
Daniel <danielbarros338@gmail.com>
2023-08-28 15:40:13 -0300

    criada store crypto

--- store/index.js
+++ store/index.js
@@ -1,5 +1,7 @@
 import { defineStore } from "pinia";
 
 import StocksStore from "./StocksStore";
+import CryptoStore from "./CryptoStore";
 
 export const useStocksStore = defineStore("Stocks", StocksStore);
+export const useCryptoStore = defineStore("Crypto", CryptoStore);
